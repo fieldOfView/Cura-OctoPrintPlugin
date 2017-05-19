e01eb7639aa511fe424d3ce3b9428a4fd5a376ba
fieldOfView <aldo@fieldofview.com>
2017-05-19 18:30:30 +0200

    Update version to reflect 2.6 branch

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ def getMetaData():
         "plugin": {
             "name": "OctoPrint connection",
             "author": "fieldOfView",
-            "version": "master",
+            "version": "2.6",
             "description": catalog.i18nc("@info:whatsthis", "Allows sending prints to OctoPrint and monitoring the progress"),
             "api": 3
         }
